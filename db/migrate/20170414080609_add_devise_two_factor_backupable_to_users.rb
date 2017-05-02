42d281c0e8d3993d7e8e19c6adab66f92923f20f
Chisato Hasegawa <chase0213+dev@gmail.com>
2017-05-02 12:31:22 +0000

    modify migration files adaptive to mysql"

--- db/migrate/20170414080609_add_devise_two_factor_backupable_to_users.rb
+++ db/migrate/20170414080609_add_devise_two_factor_backupable_to_users.rb
@@ -1,5 +1,5 @@
 class AddDeviseTwoFactorBackupableToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :otp_backup_codes, :string, array: true
+    add_column :users, :otp_backup_codes, :string
   end
 end
